33a138d72640ec509066ae9c2ac003121113f300
Daniel Schoch <d.schoch90@gmail.com>
2018-10-21 17:26:31 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -21,31 +21,31 @@ end
     redirect "owners/#{@owner.id}"
   end
 
-get '/owners/:id/edit' do
-  @owner = Owner.find(params[:id])
-  @pets = Pet.all
-  erb :'/owners/edit'
-end
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])
+    @pets = Pet.all
+    erb :'/owners/edit'
+  end
 
-get '/owners/:id' do
-  @owner = Owner.find(params[:id])
-  erb :'/owners/show'
-end
+  get '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    erb :'/owners/show'
+  end
 
-patch '/owners/:id' do
-  @owner = Owner.find(params[:id])
+  patch '/owners/:id' do
+    @owner = Owner.find(params[:id])
 
   ####### the following bug fix is required so that it's possible to remove ALL previous pets from owner.
-  if !params[:owner].keys.include?("pet_ids")
-  params[:owner]["pet_ids"] = []
-  end
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+    end
   ####### End of fix
 
-  @owner.update(params["owner"])
-  if !params["pet"]["name"].empty?
-    @owner.pets << Pet.create(name: params["pet"]["name"])
-  end
-  redirect "owners/#{@owner.id}"
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "owners/#{@owner.id}"
 end
 
 
